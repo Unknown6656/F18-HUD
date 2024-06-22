1c5987ca70d9a2b2182d516d676866a0ee9d9b61
Unknown6656 <Unknown6656@users.noreply.github.com>
2024-06-23 01:04:39 +0200

    added spam filtering

--- tools/icao-airport-ourairports.py
+++ tools/icao-airport-ourairports.py
@@ -8,6 +8,7 @@ DOWNLOAD_URL = 'https://davidmegginson.github.io/ourairports-data/airports.csv'
 PATH_CSV = 'tools/airports.csv'
 PATH_JS = 'js/airports.js'
 COLUMNS = ("id","icao","type","name","latitude_deg","longitude_deg","elevation_ft","continent","iso_country","iso_region","municipality","scheduled_service","gps_code","iata_code","local_code","home_link","wikipedia_link","keywords")
+IGNORE = ['duplicate', '(spam)', '(mischief)', '(test)', '(sandbox)', 'non-existant']
 
 
 os.chdir(os.path.dirname(__file__) + '/..')
@@ -22,11 +23,27 @@ with open(PATH_CSV, 'r', encoding="utf-8-sig") as f_csv:
     with open(PATH_JS, 'w', encoding="utf-8-sig") as f_js:
         f_js.write('// THIS IS AUTO-GENERATED\nconst airports={\n')
         first = True
+        unique_wgs84 = set()
 
         for row in csv.DictReader(f_csv, COLUMNS):
             if first:
                 first = False
             else:
-                f_js.write(f'"{row["icao"]}":{{"name":"{row["name"]}","lat":{row["latitude_deg"] or 0},"lon":{row["longitude_deg"] or 0},"alt":{row["elevation_ft"] or 0},"country":"{row["iso_country"]}","region":"{row["iso_region"]}"}},\n')
+                lat : str = row['latitude_deg'] or 'NaN'
+                lon : str = row['longitude_deg'] or 'NaN'
+                alt : str = row['elevation_ft'] or '0'
+                name : str = row['name'].replace('"', '\\"')
+
+                if lat == 'NaN' or lon == 'NaN':
+                    continue
+                elif any((ignore in name.lower()) for ignore in IGNORE):
+                    continue
+                elif name.lower() == 'spam':
+                    continue
+                elif (lat, lon) in unique_wgs84:
+                    continue
+                else:
+                    unique_wgs84.add((lat, lon))
+                    f_js.write(f'"{row["icao"]}":{{"name":"{name}","lat":{lat},"lon":{lon},"alt":{alt},"country":"{row["iso_country"] or "XX"}","region":"{row["iso_region"] or "XX-XX"}"}},\n')
 
         f_js.write('};\n')
